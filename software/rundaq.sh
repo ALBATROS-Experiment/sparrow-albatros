cd5cb9c0855df1ecd70253ce873b4b3af478e1f5
Stephen Fay <dcxstephen@gmail.com>
2025-04-03 14:07:06 -0400

    feat(rundaq.sh): automation for running dump_baseband and sump_spec.py scripts so that fingers dont freeze in field

--- software/rundaq.sh
+++ software/rundaq.sh
@@ -1,8 +1,19 @@
-echo "Mounding /dev/sda1 to /media/BASEBAND"
-sudo mount /dev/sda1 /media/BASEBAND
-echo "Configuring FPGA"
-/home/casper/python3-venv/bin/python configfpga.py
-echo "Spawning dump spectra process"
-/home/casper/python3-venv/bin/python dump_spectra.py
-echo "Spawning dump baseband process"
-sudo /home/casper/python3-venv/bin/python dump_baseband.py -l debug
+#!/bin/bash
+
+echo "This script will configure the fpga, run dump_baseband and dump_spec.py. Make sure that a drive is mounted and that config.ini is configured the way you want it before you proceed."
+
+read -p "Press Enter to continue..."
+
+# Get current python executable
+PYTHON_PATH=$(which python)
+
+# Run FPGA configuration
+$PYTHON_PATH configfpga.py
+
+# Create and detach from spectra screen session
+screen -dmS spec sudo $PYTHON_PATH dump_spectra.py
+
+# Create baseband screen session
+screen -dmS baseband bash -c "sleep 10 && $PYTHON_PATH set_optimal_coeffs.py && sleep 1 && sudo ./dump_baseband"
+
+echo "Done! Sessions running in background."
