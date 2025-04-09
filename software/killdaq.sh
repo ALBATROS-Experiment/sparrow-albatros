c8db37e14724d96743269aedd6cf7f3be2097042
Stephen Fay <dcxstephen@gmail.com>
2025-04-09 21:42:06 +0100

    daq automation rundaq and killdaq updated to start daq processes without hassle from user

---
+++ software/killdaq.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo pkill -f dump_baseband
+sudo pkill -f dump_spectra\.py
--- software/rundaq.sh
+++ software/rundaq.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-echo "This script will configure the fpga, run dump_baseband and dump_spec.py. Make sure that a drive is mounted and that config.ini is configured the way you want it before you proceed. Also, don't run this in sudo."
-
-read -p "Press Enter to continue..."
-
-# Get current python executable
-PYTHON_PATH=$(which python)
+# Get python interpreter path
+PYTHON_PATH=/home/casper/python3-venv/bin/python
 
 # Set the system clock
 $PYTHON_PATH set_gps_clock.py
 
+echo "This script will configure the fpga, run dump_baseband and dump_spec.py. Make sure that a drive is mounted and that config.ini is configured the way you want it before you proceed."
+
+read -p "Press Enter to continue..."
+
 # Run FPGA configuration
 $PYTHON_PATH configfpga.py
+echo "configfpga.py executed"
+
+echo "Waiting 10 seconds for spectra to accumulate"
+sleep 10 
 
 # Create and detach from spectra screen session
 screen -dmS spec sudo $PYTHON_PATH dump_spectra.py
+echo "screen: dumpm spectra launched"
 
 # Create baseband screen session
-screen -dmS baseband bash -c "sleep 10 && $PYTHON_PATH set_optimal_coeffs.py && sleep 1 && sudo ./dump_baseband"
+screen -dmS baseband bash -c "$PYTHON_PATH set_optimal_coeffs.py && sleep 1 && sudo ./dump_baseband"
+echo "screen: dump baseband launched"
 
 echo "Done! Sessions running in background."
