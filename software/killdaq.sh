b6fb8be9c68d6f0e9b410846709898266c2a9a57
Stephen Fay <dcxstephen@gmail.com>
2025-04-10 17:52:26 +0100

    feat(killdaq.sh, rundaq.sh, crontab-example) automation for running the daq

--- software/killdaq.sh
+++ software/killdaq.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+echo "$(date), running killdaq"
 sudo pkill -f dump_baseband
 sudo pkill -f dump_spectra\.py
--- software/rundaq.sh
+++ software/rundaq.sh
@@ -1,28 +1,54 @@
 #!/bin/bash
 
+echo "This script will configure the fpga, run dump_baseband and dump_spec.py. Make sure that a drive is mounted and that config.ini is configured the way you want it before you proceed. Run this script in sudo."
+
 # Get python interpreter path
 PYTHON_PATH=/home/casper/python3-venv/bin/python
+
+# cd into the DAQ software directory
+DAQ_PATH="/home/casper/sparrow-albatros/software"
+cd $DAQ_PATH
+echo pwd
 
 # Set the system clock
 $PYTHON_PATH set_gps_clock.py
 
-echo "This script will configure the fpga, run dump_baseband and dump_spec.py. Make sure that a drive is mounted and that config.ini is configured the way you want it before you proceed."
+# Default config file
+CONFIG_FILE="config.ini"
 
-read -p "Press Enter to continue..."
+# Parse command line arguments
+while getopts "c:" opt; do
+    case $opt in 
+        c)
+            CONFIG_FILE="$OPTARG"
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+    esac
+done
+
+echo "Using config file: $CONFIG_FILE"
 
 # Run FPGA configuration
-$PYTHON_PATH configfpga.py
-echo "configfpga.py executed"
+echo "\nRunning configfpga.py with config file ${CONFIG_FILE}"
+$PYTHON_PATH configfpga.py -c $CONFIG_FILE
 
-echo "Waiting 10 seconds for spectra to accumulate"
+echo "\nWaiting 10 seconds for spectra to accumulate..."
 sleep 10 
+echo "done waiting, now running dump_spectra.py and dump_baseband"
 
 # Create and detach from spectra screen session
 screen -dmS spec sudo $PYTHON_PATH dump_spectra.py
 echo "screen: dumpm spectra launched"
-
+ 
 # Create baseband screen session
 screen -dmS baseband bash -c "$PYTHON_PATH set_optimal_coeffs.py && sleep 1 && sudo ./dump_baseband"
 echo "screen: dump baseband launched"
+
+#$PYTHON_PATH set_optimal_coeffs.py
+#sleep 1
+#sudo ./dump_baseband
 
 echo "Done! Sessions running in background."
